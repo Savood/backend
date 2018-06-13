49e9bd4899d3fe9f46f6c6b5ca0d4741a0812554
zachs <sebastian.ber.zach@hpe.com>
2018-06-13 11:28:24 +0200

    test database connection in gitlab

---
+++ database/database.go
@@ -0,0 +1,48 @@
+package database
+
+import (
+	"github.com/globalsign/mgo"
+	"os"
+)
+
+var db *mgo.Database
+
+// ConnectDatabase establishes connection to database
+func ConnectDatabase(connectionUrl, databaseName *string) error {
+
+	if connectionUrl == nil {
+		s := os.Getenv("MONGODB_URL")
+		connectionUrl = &s
+	}
+	if databaseName == nil {
+		s := os.Getenv("MONGODB_DB")
+		databaseName = &s
+	}
+
+	session, err := mgo.Dial(*connectionUrl)
+	if err != nil {
+		return err
+	}
+	db = session.DB(*databaseName)
+
+	// TODO do this properly with right data
+	/*
+	db.C("users").EnsureIndex(mgo.Index{
+		Key:    []string{"email"},
+		Unique: true,
+	})
+	db.C("users").EnsureIndex(mgo.Index{
+		Key:    []string{"username"},
+		Unique: true,
+	})
+	*/
+
+
+	return err
+
+}
+
+// GetDatabase returns database connection object
+func GetDatabase() *mgo.Database {
+	return db
+}
